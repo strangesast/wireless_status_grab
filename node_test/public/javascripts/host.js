972956d0f6d93e2deb0bba0ab1458ac720a119ea
Sam Zagrobelny <strangesast@gmail.com>
2016-01-21 13:38:38 -0500

    minor

--- node_test/public/javascripts/host.js
+++ node_test/public/javascripts/host.js
@@ -1,5 +1,5 @@
-records_loc = window.location.pathname.split('/').concat('records').join('/')
-records_canvas = document.getElementById('records-canvas');
+var records_loc = window.location.pathname.split('/').concat('records').join('/');
+var records_canvas = document.getElementById('records-canvas');
 
 minmax = function(iterable, key) {
   var min = iterable.reduce(function(a, b) {
@@ -11,22 +11,32 @@ minmax = function(iterable, key) {
   return {
     min: min[key],
     max: max[key]
-  }
-}
+  };
+};
 
-var localdata = {}
+var localdata = {};
 
 makeRequest(records_loc, 'GET', null).then(function(result) {
   var records = JSON.parse(result.response);
   localdata.records = records;
-  recalculateCanvas(records_canvas);
+  recalculateCanvas();
 
 }, function(error) {
   console.log(error);
 });
 
-recalculateCanvas = function(canvas_element) {
-  var _parent = canvas_element.parentElement;
+canvasMouseoverListener = function(e) {
+  var rect = records_canvas.getBoundingClientRect();
+  var xpos = e.clientX - rect.left,
+      ypos = e.clientY - rect.top;
+
+  console.log(xpos, ypos);
+};
+
+var lastListener = null;
+
+recalculateCanvas = function() {
+  var _parent = records_canvas.parentElement;
   //option1
   //var dimensions = _parent.getBoundingClientRect();
   //var width = dimensions.width;
@@ -38,8 +48,8 @@ recalculateCanvas = function(canvas_element) {
   var width = _parent.clientWidth - 30;
   console.log(width);
   var height = 100; // dimensions.height;
-  canvas_element.width = width;
-  canvas_element.height = height;
+  records_canvas.width = width;
+  records_canvas.height = height;
 
   var records = localdata.records;
   if (records.length < 1) {
@@ -51,7 +61,8 @@ recalculateCanvas = function(canvas_element) {
 
   var records_props = minmax(records, 'datetime');
   var signal_props = minmax(records, 'signal_strength');
-  var bins = []
+  records_props.max = new Date().getTime() / 1000;
+  var bins = [];
 
   ctx.beginPath();
   ctx.lineTo(0, height);
@@ -74,7 +85,7 @@ recalculateCanvas = function(canvas_element) {
       ctx.lineTo(px*width, height);
       ctx.closePath();
       ctx.fillStyle = "grey";
-      ctx.fill()
+      ctx.fill();
       ctx.strokeStyle = "black";
       ctx.beginPath();
       reset = true;
@@ -82,8 +93,10 @@ recalculateCanvas = function(canvas_element) {
       ctx.stroke();
     }
   }
-}
+  records_canvas.removeEventListener('mousemove', canvasMouseoverListener);
+  records_canvas.addEventListener('mousemove', canvasMouseoverListener);
+};
 
 window.addEventListener('resize', function(e) {
-  recalculateCanvas(records_canvas);
+  recalculateCanvas();
 });
